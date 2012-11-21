8aa94741b4729ecc9bd5ad79c33cb9aff20ea6e3
Sebastian Dieser <sebastiandieser@Sebastians-MacBook-Pro.local>
2012-11-21 11:30:28 -0800

    New branch to test stuff without Jared bitching about some decisions.

--- config.rb
+++ config.rb
@@ -7,6 +7,9 @@ sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
 
+# Get more precise %
+Sass::Script::Number.precision = 10
+
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
